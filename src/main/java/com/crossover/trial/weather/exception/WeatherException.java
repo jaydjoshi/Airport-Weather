e3e4ea15754088d6de0c1cd93b4fcca690335c33
Jay Joshi <joshi.jay.d@gmail.com>
2016-09-24 19:27:23 +0530

    Implementation of WeatherException

--- src/main/java/com/crossover/trial/weather/exception/WeatherException.java
+++ src/main/java/com/crossover/trial/weather/exception/WeatherException.java
@@ -1,7 +1,45 @@
-package com.crossover.trial.weather;
+package com.crossover.trial.weather.exception;
+
+import com.crossover.trial.weather.model.DataPointType;
 
 /**
  * An internal exception marker
+ * implemented custom WeatherException extends Exception
+ * 
  */
 public class WeatherException extends Exception {
+
+	/**
+	 * generated serial id
+	 */
+	private static final long serialVersionUID = -1763593411470554908L;
+
+	private final DataPointType dataPointType;
+
+    public WeatherException() {
+    	super();
+    	this.dataPointType=null;
+	}
+
+	public WeatherException(DataPointType dataPointType, String message) {
+		super(message);
+		this.dataPointType = dataPointType;
+	}
+
+	public WeatherException(DataPointType dataPointType, String message, Throwable cause) {
+		super(message, cause);
+		this.dataPointType = dataPointType;
+	}
+
+	public DataPointType getDataPointType() {
+		return dataPointType;
+	}
+
+	@Override
+	public String getMessage() {
+		return super.getMessage() + " for data type :" + dataPointType.name();
+	}
+
+
+
 }
